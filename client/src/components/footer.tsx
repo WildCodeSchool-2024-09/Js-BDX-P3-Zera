29c26ad58238455ebad61fbd7661bea4949d3831
Florian Minicelle <fminicelle@gmail.com>
2025-01-10 10:49:36 +0100

    footer + header logos added

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,18 +1,51 @@
 import "./Footer.css";
 import "../components/Variables.css";
+import linkedinImage from "../assets/images/linkedin.png";
+import twitterImage from "../assets/images/twitter.jpg";
+import instagramImage from "../assets/images/instagram.png";
 
 function Footer() {
   return (
     <footer className="footer">
       <nav className="footer-nav">
-        <a href="#facebook" className="footer-link" aria-label="Facebook">
-          📘 Facebook
+        <a
+          href="#linkedin"
+          target="_blank"
+          rel="noreferrer"
+          className="footer-link"
+          aria-label="Linkedin Profile"
+        >
+          <img
+            src={linkedinImage}
+            alt="LinkedIn"
+            className="footer-icon"
+          />
         </a>
-        <a href="#twitter" className="footer-link" aria-label="Twitter">
-          🐦 Twitter
+        <a
+          href="#X"
+          className="footer-link"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="X"
+        >
+          <img
+            src={twitterImage} 
+            alt="X"
+            className="footer-icon"
+          />
         </a>
-        <a href="#instagram" className="footer-link" aria-label="Instagram">
-          📸 Instagram
+        <a
+          href="#instagram"
+          className="footer-link"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Instagram"
+        >
+          <img
+            src={instagramImage} 
+            alt="Instagram"
+            className="footer-icon"
+          />
         </a>
       </nav>
     </footer>
@@ -20,3 +53,4 @@ function Footer() {
 }
 
 export default Footer;
+
