3638c4d094b0fc06d0b0dfdef9288bd7fcf3bade
Wakoz <julien.cartron.pro@gmail.com>
2025-01-11 13:47:23 +0100

    Add books modules, after test in Postman all works. Add glossary table and fix other table

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,4 +1,4 @@
-create table registers (
+create table users (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
   password varchar(255) not null
@@ -6,27 +6,21 @@ create table registers (
 
 create table clients (
   id int unsigned primary key auto_increment not null,
-  registers_id int unsigned not null,
-  foreign key(registers_id) references registers(id)
+  users_id int unsigned not null,
+  foreign key(users_id) references users(id)
 );
 
 create table admins (
   id int unsigned primary key auto_increment not null,
-  registers_id int unsigned not null,
-  foreign key(registers_id) references registers(id)
-);
-
-create table contains (
-  id int unsigned primary key auto_increment not null,
-  name varchar(255) not null
+  users_id int unsigned not null,
+  foreign key(users_id) references users(id)
 );
 
 create table books (
   id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
   resume text not null,
-  illu varchar(255) not null,
-  contains_id int unsigned not null,
-  foreign key(contains_id) references contains(id)
+  illustration varchar(255) not null
 );
 
 create table books_clients (
@@ -39,7 +33,7 @@ create table books_clients (
   foreign key(clients_id) references clients(id)
 );
 
-create table links (
+create table choices (
   id int unsigned primary key auto_increment not null,
   text text not null,
   path varchar(255) not null
@@ -47,28 +41,27 @@ create table links (
 
 create table episodes (
   id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  types varchar(255) not null,
   is_free boolean default false,
   to_register boolean default false,
   books_id int unsigned not null,
-  foreign key(books_id) references books(id),
-  contains_id int unsigned not null,
-  foreign key(contains_id) references contains(id)
+  foreign key(books_id) references books(id)
 );
 
-create table episodes_links (
+create table episodes_choices (
   episodes_id int unsigned not null,
   foreign key(episodes_id) references episodes(id),
-  links_id int unsigned not null,
-  foreign key(links_id) references links(id)
+  choices_id int unsigned not null,
+  foreign key(choices_id) references choices(id)
 );
 
-create table illu (
+create table illustrations (
   id int unsigned primary key auto_increment not null,
-  url varchar(255) not null,
+  urls varchar(255) not null,
   episodes_id int unsigned not null,
   foreign key(episodes_id) references episodes(id)
 );
-
 
 create table paragraphs (
   id int unsigned primary key auto_increment not null,
@@ -77,10 +70,16 @@ create table paragraphs (
   foreign key(episodes_id) references episodes(id)
 );
 
-create table save (
-  default current_timestamp,
+create table progressions (
+  dates TIMESTAMP default current_timestamp,
   episodes_id int unsigned not null,
   foreign key(episodes_id) references episodes(id),
   clients_id int unsigned not null,
   foreign key(clients_id) references clients(id)
 );
+
+create table glossary (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  definition text not null
+)
